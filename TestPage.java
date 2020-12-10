879aa55b72cbdba72dfb1caea50c9982f09f4e59
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2020-12-10 14:43:42 +0900

    chapter10SortTest_linkPrint

---
+++ TestPage.java
@@ -0,0 +1,21 @@
+package object;
+
+import java.util.Scanner;
+
+public class TestPage {
+	public static void main(String[] args) {
+		boolean run = true;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("프로그램을 시작! 종료를 원하시면  \"종료\"를 입력하세요.");
+		
+		while(run) {
+			System.out.println("입력하세요 : ");
+			String user = scanner.nextLine();
+			System.out.println("'" +user +"'을 입력하셨습니다.");
+			if(user.equals("종료")) {
+				System.out.println("\n 프로그램을 종료하겠습니다.");
+				run = false;
+			}
+		}
+	}
+}
